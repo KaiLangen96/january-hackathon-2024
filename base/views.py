084836661d9db4d022cd70f2e547dff30a8bacc9
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-01-19 16:46:20 +0000

    Added demo dashboard

--- base/views.py
+++ base/views.py
@@ -5,6 +5,8 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+
 
 from .models import Transaction, Category, SavingGoal, SavingsDeposit
 from .forms import TransactionForm, CategoryForm, SavingGoalForm, SavingsDepositForm
@@ -28,18 +30,29 @@ class HomePageView(generic.View):
 
 class TrackerPageView(generic.View):
     """
+
     Basic homepage view.
 
     """
+    template_name = "tracker.html"
+
     def get(self, request):
         """
         Basic Get view for the homepage.
 
         """
-        return render(
-            request,
-            "tracker.html",
-        )
+
+        all_users = User.objects.all()
+        user = request.user
+
+        context = {
+            'all_users': all_users,
+            'user': user,
+        }
+
+
+        return render(request, self.template_name, context)
+
 
 class AboutPageView(generic.View):
     """
@@ -244,3 +257,32 @@ class SavingGoalDeleteView(DeleteView):
     model = SavingGoal
     success_url = reverse_lazy('transaction_list')  # Redirect to the transaction list after deletion
     template_name = 'saving_goal_confirm_delete.html'  # Create this template if it doesn't exist
+
+
+
+def handler404(request, exception):
+    """
+    Custom 404 page
+    """
+    return render(request, "error_html/404.html", status=404)
+
+
+def handler500(request):
+    """
+    Custom 500 page
+    """
+    return render(request, "error_html/500.html", status=500)
+
+
+def handler403(request, exception):
+    """
+    Custom 403 page
+    """
+    return render(request, "error_html/403.html", status=403)
+
+
+def handler405(request, exception):
+    """
+    Custom 405 page
+    """
+    return render(request, "error_html/405.html", status=405)
