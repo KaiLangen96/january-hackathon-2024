fee61575c0a9bff310bfad65977f8fcb9b790dac
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-01-22 01:04:55 +0000

    Added template for all goals

--- base/models.py
+++ base/models.py
@@ -40,6 +40,7 @@ class SavingGoal(models.Model):
     name = models.CharField(max_length=255)
     target_amount = models.DecimalField(max_digits=10, decimal_places=2)
     current_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
